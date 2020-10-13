2ead53aecbfb094efd5ea1975f16dcab22346875
recurseDev <manmohan__1896@yahoo.com>
2020-10-13 02:21:12 -0300

    test change

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
@@ -22,6 +21,7 @@ if (process.env.NODE_ENV === "production") {
 		res.sendFile(path.join(__dirname, "client/build", "index.html"));
 	});
 }
+
 
 app.listen(port, (error) => {
 	if (error) throw error;
