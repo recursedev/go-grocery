b56f2ba739104b10cde49d51e80e2442ed1cf0e9
recurseDev <manmohan__1896@yahoo.com>
2020-10-13 01:51:49 -0300

    2nd commit to fix package.json file settings

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,7 @@
 import {createStore, applyMiddleware} from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+
 import createSagaMiddleware from 'redux-saga';
 
 import rootSaga from './root-saga'
@@ -10,7 +10,11 @@ import rootReducer from './root-reducer'
 
 const sagaMiddleware = createSagaMiddleware();
 
-const middlewares = [thunk, logger, sagaMiddleware];
+const middlewares = [sagaMiddleware];
+
+if (process.env.NODE_ENV === "development") {
+	middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares))
 
