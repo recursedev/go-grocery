eefec102b891855b488fa83e77ae980b6b704dfd
recurseDev <manmohan__1896@yahoo.com>
2020-10-18 17:17:18 -0300

    adding compressionlibrary for gzipping on heroku

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const stripe = require("stripe")(process.env.STRIPE_FALTU_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(conpression());
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
